39132aeb7086231994511e8c9c755be04b5c674b
ono <ono.oumar@hotmail.fr>
2019-05-16 13:10:59 +0000

    Test et ajout de quelques lignes. -Menu -afichage du menu choisi

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -5,6 +5,11 @@ public class Order {
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
+        System.out.println("Choix du menu :  ");
+        System.out.println("1 ==> Poulet");
+        System.out.println("2 => Boeuf");
+        System.out.println("3 => Végétarien");
+        System.out.println("Que souhaitez-vous comme menu ?");
 
     }
     /**
@@ -12,6 +17,7 @@ public class Order {
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
+        System.out.println("Vous avez choisi le menu " + nbMenu);
 
     }
 }
