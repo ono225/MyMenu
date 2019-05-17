61d522e41f6c73cd26d821828d0729979b0fce3c
ono <ono.oumar@hotmail.fr>
2019-05-17 17:55:28 +0000

    Gestion menu -- accompagnement -- boisson

--- test/com/ocr/anthony/OrderTest.java
+++ test/com/ocr/anthony/OrderTest.java
@@ -77,7 +77,7 @@ public class OrderTest {
 
     }
 
-    @Test
+  /*  @Test
 
     public void Given_ChikenInStandardInput_When_MenuIsRun_Then_DisplayCorrectProcess() {
 
@@ -93,6 +93,36 @@ public class OrderTest {
 
         assertEquals(output.length() > "Vous avez choisi comme menu : poulet\n".length(), true);
 
+    }*/
+
+    @Test
+    public void Given_ChikenWithFriesAndWaterInStandardInput_When_MenuIsRun_Then_DisplayCorrectProcess() {
+        System.setIn(new ByteArrayInputStream("1\n2\n3\n".getBytes()));
+        order = new Order();
+        order.runMenu();
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals("Vous avez choisi comme menu : poulet", output[5]);
+        assertEquals("Vous avez choisi comme accompagnement : frites", output[11]);
+        assertEquals("Vous avez choisi comme boisson : soda", output[17]);
+    }
+    @Test
+    public void Given_BeefWithVegetableInStandardInput_When_MenuIsRun_Then_DisplayCorrectProcess() {
+        System.setIn(new ByteArrayInputStream("2\n1\n".getBytes()));
+        order = new Order();
+        order.runMenu();
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals("Vous avez choisi comme menu : boeuf", output[5]);
+        assertEquals("Vous avez choisi comme accompagnement : légumes frais", output[11]);
+    }
+    @Test
+    public void Given_VegetarianWithNoRiceAndSparklingWaterInStandardInput_When_MenuIsRun_Then_DisplayCorrectProcess() {
+        System.setIn(new ByteArrayInputStream("3\n2\n2\n".getBytes()));
+        order = new Order();
+        order.runMenu();
+        String[] output = outContent.toString().replace("\r\n", "\n").split("\n");
+        assertEquals("Vous avez choisi comme menu : végétarien", output[5]);
+        assertEquals("Vous avez choisi comme accompagnement : pas de riz", output[10]);
+        assertEquals("Vous avez choisi comme boisson : eau gazeuse", output[16]);
     }
 
     @Test
